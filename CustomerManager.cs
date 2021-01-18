6a68399dc72223d81fc9e75e8ab71571c6f25d3a
emreharun <50295357+emreharun@users.noreply.github.com>
2021-01-18 03:21:09 +0300

    Add files via upload

---
+++ CustomerManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Customer Added: " + customer.Name + " " + customer.Surname);
+        }
+        public void List(Customer customer)
+        {
+            Console.WriteLine("Customer List: " + customer.Name + " " +customer.Surname);
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Customer Deleted: " + customer.Name + " " + customer.Surname);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Name = "A";
+            customer1.Surname = "B";
+            customer1.Id = 1;
+
+            Customer customer2 = new Customer();
+            customer2.Name = "C";
+            customer2.Surname = "D";
+            customer2.Id = 2;
+
+            CustomerManager customerManager = new CustomerManager();
+            Console.WriteLine("ADDED");
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            Console.WriteLine("DELETED");
+            customerManager.Delete(customer1);
+            customerManager.Delete(customer2);
+            Console.WriteLine("LIST");
+            customerManager.List(customer1);
+            customerManager.List(customer2);
+
+        }
+    }
+}
